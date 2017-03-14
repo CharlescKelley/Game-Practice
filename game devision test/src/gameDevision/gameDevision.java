a30dc01bc2ece24de3fe1bbf5aaceaddea3d316c
CharlescKelley <charlesckelley@gmail.com>
2017-03-14 08:54:29 -0500

    Add files via upload
    haven't done much with this, like at all but i will when i get back in town

---
+++ game devision test/src/gameDevision/gameDevision.java
@@ -0,0 +1,27 @@
+package gameDevision;
+
+import java.util.Scanner;
+
+public class gameDevision {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter int: \n");
+		int i = in.nextInt();
+		
+		int[] dividen;
+		switch(i)
+		{
+		case 1:
+			dividen = {0,1,2,3,4,5,6,7,8,9};
+		case 2:
+			
+		case 3:
+			
+		case 4:
+			
+		}
+			
+	}
+
+}
